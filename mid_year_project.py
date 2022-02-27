ce799b12bb5125b17bee121fc263af77810c79fc
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-02-27 16:41:28 -0500

    Save Midyear Project

--- mid_year_project.py
+++ mid_year_project.py
@@ -17,6 +17,24 @@ def chutes_and_ladders():
     if ladders1 == chutes3 & chutes3 != 6:
         ladders1 += 1
     else: ladders1 -= 1
+    if ladders2 == chutes1 & chutes1 != 6:
+        ladders1 += 1
+    else: ladders2 -= 1
+    if ladders2 == chutes2 & chutes2 != 6:
+        ladders2 += 1
+    else: ladders2 -= 1
+    if ladders2 == chutes3 & chutes3 != 6:
+        ladders2 += 1
+    else: ladders2 -= 1
+    if ladders3 == chutes1 & chutes1 != 6:
+        ladders3 += 1
+    else: ladders3 -= 1
+    if ladders3 == chutes2 & chutes2 != 6:
+        ladders3 += 1
+    else: ladders3 -= 1
+    if ladders3 == chutes3 & chutes3 != 6:
+        ladders3 += 1
+    else: ladders3 -= 1
     
     dice_roll = int(input("Pick a number between 1 and 6."))
 
